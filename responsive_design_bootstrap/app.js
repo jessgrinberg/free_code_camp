661aeb8ff702d0a4944b4f467472301f500680ee
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-20 09:16:32 -0700

    changing the css of an element using jquery

--- responsive_design_bootstrap/app.js
+++ responsive_design_bootstrap/app.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
     // $(".well").addClass("animated shake");
     // $("#target3").addClass("animated fadeOut");
 
-    $("button").addClass("animated");
-    $(".btn").addClass("shake");
-    $("#target1").addClass("btn-primary");
-    $("button").removeClass("btn-default");
+    // $("button").addClass("animated");
+    // $(".btn").addClass("shake");
+    // $("#target1").addClass("btn-primary");
+    // $("button").removeClass("btn-default");
+
+    $("#target1").css("color","red");
 
 });
