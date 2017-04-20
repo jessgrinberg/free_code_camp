3e2fed5fe20111126b7684124b6299d4b6e25011
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-20 09:15:13 -0700

    remove classes from an element

--- responsive_design_bootstrap/app.js
+++ responsive_design_bootstrap/app.js
@@ -7,4 +7,6 @@ $(document).ready(function() {
     $("button").addClass("animated");
     $(".btn").addClass("shake");
     $("#target1").addClass("btn-primary");
+    $("button").removeClass("btn-default");
+
 });
