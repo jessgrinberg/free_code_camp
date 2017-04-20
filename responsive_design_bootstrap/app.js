b8cb18179bdd2fb9a70d862aaff3db4767e93c31
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-20 09:35:32 -0700

    target even numbered elements using jQuery

--- responsive_design_bootstrap/app.js
+++ responsive_design_bootstrap/app.js
@@ -18,5 +18,6 @@ $(document).ready(function() {
     $("#target1").parent().css("background-color", "red");
     $("#right-well").children().css("color", "orange");
     $(".target:nth-child(2)").addClass("animated bounce")
+    $(".target:even").addClass("animated shake");
 
 });
