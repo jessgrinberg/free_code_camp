58ee0a8ee81b6ebef4b11347deb9c9507fce455a
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-24 15:41:58 -0700

    understanding unintialized variable

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -14,3 +14,18 @@ a = 7;
 b = a;
 
 var initializeVar = 9;
+
+// Initialize these three variables
+var d;
+var e;
+var f;
+
+// Do not change code below this line
+
+d = 5;
+e = 10;
+f = "I am a";
+
+d = d + 1;
+e = e + 5;
+f = f + " String!";
