b814cdea80b0c684525bc04e78bd270e4ade94b8
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-26 00:11:38 -0700

    local scope and functions

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -249,3 +249,18 @@ function fun2() {
   }
   console.log(output);
 }
+
+
+function myLocalScope() {
+  'use strict';
+  var myVar = 'foo';
+
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log(myVar);
+
+// Now remove the console log line to pass the test
