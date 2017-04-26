9d43d4f19582336c28036e7bca56e6b63b25f431
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-26 00:13:14 -0700

    gloabl vs local scope in functions

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -264,3 +264,19 @@ myLocalScope();
 console.log(myVar);
 
 // Now remove the console log line to pass the test
+
+
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
