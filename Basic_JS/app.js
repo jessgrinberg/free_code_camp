b029cc39a03a28acfe6133d813190566ff72529a
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-26 15:47:43 -0700

    understanding booleans

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -311,3 +311,13 @@ var testArr = [1,2,3,4,5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6)); // Modify this line to test
 console.log("After: " + JSON.stringify(testArr));
+
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
