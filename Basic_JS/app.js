bf89761b7a065fcf988f6ade66f8ed5837e5a51f
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-27 08:23:00 -0700

    replacing If Else Chains with Switch

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -636,3 +636,30 @@ function sequentialSizes(val) {
 }
 
 sequentialSizes(1);
+
+
+
+function chainToSwitch(val) {
+  var answer = "";
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+
+// Change this value to test
+chainToSwitch(7);
