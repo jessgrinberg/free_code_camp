b4a37f2f628762b1d3d5782828fd6208164b423d
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-26 18:00:33 -0700

    comparison with the greater than or equal to operator

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -388,3 +388,20 @@ function testGreaterThan(val) {
 }
 
 testGreaterThan(10);
+
+
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  
+    return "20 or Over";
+  }
+
+  if (val >= 10 ) {
+    return "10 or Over";
+  }
+
+  return "9 or Under";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
