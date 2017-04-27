36752ce384045ee2eb5ed8a85b567572140a1cf1
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-27 08:30:12 -0700

    return early pattern for functions

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -672,3 +672,15 @@ function isLess(a, b) {
 }
 // Change these values to test
 isLess(10, 15);
+
+
+
+function abTest(a, b) {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
