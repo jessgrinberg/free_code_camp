5eec568c5844f6862fc1da3e2710b4bf279a780d
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-27 07:59:43 -0700

    selecting from many options with switch statements

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -558,3 +558,28 @@ function golfScore(par, strokes) {
 
 // Change these values to test
 golfScore(5, 4);
+
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+
+    case 3:
+      return "gamma";
+      break;
+
+    case 4:
+      return "delta";
+      break;
+  }
+   return answer;
+}
+// Change this value to test
+caseInSwitch(1);
