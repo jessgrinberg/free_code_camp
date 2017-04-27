648493a75fd7636544bae92ce9c8cec687f882a1
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-26 18:39:12 -0700

    chaining If Else Statements

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -510,3 +510,25 @@ function orderMyLogic(val) {
 
 // Change this value to test
 orderMyLogic(7);
+
+
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
+  } else {
+      return "Change Me";
+  }
+}
+
+// Change this value to test
+testSize(7);
