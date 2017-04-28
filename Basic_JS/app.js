6cd56220c778e777a3cff5452fe71b34e90cf207
Jessica Grinberg <jessgrinberg@gmail.com>
2017-04-28 13:04:46 -0700

    iterate through an array with a for loop

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -831,3 +831,10 @@ var myArray = [];
 for(var i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
+
+
+var myArr = [ 2, 3, 4, 5, 6];
+var total = 0;
+for (var i=0; i<myArr.length; i++) {
+  total += myArr[i];
+}
