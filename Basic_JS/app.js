8afe5051fe3d1a44d41d2339a9d95bba597b3d79
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-01 15:11:30 -0700

    find numbers with regular expressions

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -871,8 +871,17 @@ var testString = "Ada Lovelace and Charles Babbage designed the first computer a
 // Example
 var expressionToGetSoftware = /software/gi;
 var softwareCount = testString.match(expressionToGetSoftware).length;
-  
+
 var expression = /and/gi;  // Change this Line
 
 // This code counts the matches of expression in testString
 var andCount = testString.match(expression).length;
+
+
+// Setup
+var testString = "There are 3 cats but 4 dogs.";
+
+var expression = /\d+/g;  // Change this line
+
+// This code counts the matches of expression in testString
+var digitCount = testString.match(expression).length;
