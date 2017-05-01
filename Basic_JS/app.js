ce07cb59fe8ebeb15b68ea9e74b803a0ce6a4e48
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-01 15:12:42 -0700

    find whitespace with regular expressions

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -885,3 +885,12 @@ var expression = /\d+/g;  // Change this line
 
 // This code counts the matches of expression in testString
 var digitCount = testString.match(expression).length;
+
+
+// Setup
+var testString = "How many spaces are there in this sentence?";
+
+var expression = /\s+/g;  // Change this line
+
+// This code counts the matches of expression in testString
+var spaceCount = testString.match(expression).length;
