07810ba0ebc2f6c9679fec310e6c393ddab23c3e
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-01 16:47:49 -0700

    testing objects for properties

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -921,8 +921,27 @@ function phoneticLookup(val) {
 
   result = lookup[val];
 
-  return result; 
+  return result;
 }
 
 // Change this value to test
 phoneticLookup("charlie");
+
+
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  if (myObj.hasOwnProperty(checkProp)) {
+    return myObj[checkProp];
+  } else {
+    return 'Not Found';
+  }
+}
+
+// Test your code by modifying these values
+checkObj("gift");
