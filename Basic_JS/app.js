53896c3fbb1ef086d682aacfff3f35e18fc78488
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-01 16:29:11 -0700

    using objects for lookups

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -902,3 +902,27 @@ var expression = /\S/g;  // Change this line
 
 // This code counts the matches of expression in testString
 var nonSpaceCount = testString.match(expression).length;
+
+
+
+// Setup
+function phoneticLookup(val) {
+
+  var result = "";
+
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+
+  result = lookup[val];
+
+  return result; 
+}
+
+// Change this value to test
+phoneticLookup("charlie");
