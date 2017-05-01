e46c303b7daea67cd1c8f108779cc81f3ecd938d
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-01 12:22:26 -0700

    generate random whole numbers with JS

--- Basic_JS/app.js
+++ Basic_JS/app.js
@@ -851,3 +851,9 @@ while (i < 5) {
 function randomFraction() {
   return Math.random();
 }
+
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
