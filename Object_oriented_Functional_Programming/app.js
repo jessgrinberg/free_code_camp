fcbb0043721bc61a6fc891d54210fbae570bdaaf
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-02 10:32:10 -0700

    Filter Arrays with filter

--- Object_oriented_Functional_Programming/app.js
+++ Object_oriented_Functional_Programming/app.js
@@ -99,3 +99,11 @@ var singleVal = 0;
 singleVal = array.reduce(function(previousVal,currentVal) {
   return previousVal + currentVal;
 });
+
+//8.Filter Arrays with filter
+
+var oldArray = [1,2,3,4,5,6,7,8,9,10];
+
+var newArray = oldArray.filter(function(val) {
+  return val < 6;
+});
