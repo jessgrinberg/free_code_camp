d236fb8bf946958ac37eb8562c90268bc631c1a5
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-01 21:48:21 -0700

    Declare JavaScript Objects as Variables

--- Object_oriented_Functional_Programming/app.js
+++ Object_oriented_Functional_Programming/app.js
@@ -1,7 +1,12 @@
-//1. Build JS Objects 
-var myDog = {
-  "name": "Vegas",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["cali","chicago","portland"]
+//1. Declare JavaScript Objects as Variables
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+var motorBike = {
+  "wheels": 2,
+  "engines": 1,
+  "seats": 2
 };
