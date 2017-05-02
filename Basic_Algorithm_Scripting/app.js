95da9a0c047f17a3430612dbabacb613cc25aabc
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-02 11:14:21 -0700

    Factorialize a Number

--- Basic_Algorithm_Scripting/app.js
+++ Basic_Algorithm_Scripting/app.js
@@ -6,3 +6,15 @@ function reverseString(str) {
 }
 
 reverseString("Greetings from Earth");
+
+
+//2.Factorialize a Number
+
+function factorialize(num) {
+  if(num === 0) {
+    return 1;
+  }
+  return num * factorialize( num - 1);
+}
+
+factorialize(5);
