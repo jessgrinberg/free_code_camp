401b77dacf8c6bf42cbbb148324fea1fd5607588
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-02 10:00:59 -0700

    Condense arrays with reduce

--- Object_oriented_Functional_Programming/app.js
+++ Object_oriented_Functional_Programming/app.js
@@ -89,3 +89,13 @@ var oldArray = [1,2,3,4,5];
 var newArray = oldArray.map(function(val) {
   return val +3;
 });
+
+
+//7.Condense arrays with reduce
+
+var array = [4,5,6,7,8];
+var singleVal = 0;
+
+singleVal = array.reduce(function(previousVal,currentVal) {
+  return previousVal + currentVal;
+});
