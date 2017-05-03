0f042c3057605a607346ccf51f7e69c727728c09
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-03 13:41:53 -0700

    function confirmEnding(str, target) {

--- Basic_Algorithm_Scripting/app.js
+++ Basic_Algorithm_Scripting/app.js
@@ -94,3 +94,12 @@ function largestOfFour(arr) {
   return largestArray;
 }
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+//7. Confirm the Ending
+
+function confirmEnding(str, target) {
+  return (str.substr(-target.length) === target);
+}
+
+confirmEnding("Bastian", "n");
