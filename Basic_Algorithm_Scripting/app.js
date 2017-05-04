918e91abe8326ca362c7febbe7ce290d1786575e
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-04 08:58:20 -0700

    Chunky Monkey

--- Basic_Algorithm_Scripting/app.js
+++ Basic_Algorithm_Scripting/app.js
@@ -133,3 +133,16 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+
+//10.Chunky Monkey
+
+function chunkArrayInGroups(arr, size) {
+  var result = [];
+  for (var i=0; i<arr.length; i+=size) {
+    result.push(arr.slice(i, i+size));
+  }
+  return result;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
