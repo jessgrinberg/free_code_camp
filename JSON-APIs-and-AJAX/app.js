eee571b3babd3c089906022e050a1983ded87b35
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-05 15:13:17 -0700

    Change Text with Click Events

--- JSON-APIs-and-AJAX/app.js
+++ JSON-APIs-and-AJAX/app.js
@@ -1,6 +1,7 @@
 //1. Trigger click events with jQuery
 $(document).ready(function() {
   $("#getMessage").on("click", function() {
-
+      //2. Change Text with Click Events
+      $(".message").html("Here is the message");
      })
 });
