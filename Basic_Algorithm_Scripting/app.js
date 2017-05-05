1413c68ec3f5dfefc77c26c2eb1826682ddf9764
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-05 14:26:41 -0700

    Caesars Cipher

--- Basic_Algorithm_Scripting/app.js
+++ Basic_Algorithm_Scripting/app.js
@@ -216,3 +216,22 @@ function getIndexToIns(arr, num) {
 }
 
 getIndexToIns([40, 60,20,30], 50);
+
+
+//15.Caesars Cipher
+
+function rot13(str) {
+  var newString = [];
+  for(var i = 0; i < str.length; i++) {
+    if(str.charCodeAt(i) < 65 || str.charCodeAt(i) > 90) {
+      newString.push(str.charAt(i));
+    } else if (str.charCodeAt(i) > 77) {
+      newString.push(String.fromCharCode(str.charCodeAt(i) - 13));
+    } else {
+      newString.push(String.fromCharCode(str.charCodeAt(i) + 13));
+    }
+  }
+  return newString.join("");
+}
+
+rot13("SERR PBQR PNZC");
