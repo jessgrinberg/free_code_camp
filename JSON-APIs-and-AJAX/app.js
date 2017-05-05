cce5e0ebe996e7b4bba971d180044f34cd86771e
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-05 15:17:44 -0700

    Get JSON with the jQuery getJSON Method

--- JSON-APIs-and-AJAX/app.js
+++ JSON-APIs-and-AJAX/app.js
@@ -2,6 +2,11 @@
 $(document).ready(function() {
   $("#getMessage").on("click", function() {
       //2. Change Text with Click Events
-      $(".message").html("Here is the message");
-     })
+      //$(".message").html("Here is the message");
+
+      //3.Get JSON with the jQuery getJSON Method
+    $.getJSON("/json/cats.json", function(json) {
+      $(".message").html(JSON.stringify(json));
+    });
+  })
 });
