b473e2985d30b8d1a4151f2b5bd7b85f8f0c713b
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-05 15:22:06 -0700

    Convert JSON Data to HTML

--- JSON-APIs-and-AJAX/app.js
+++ JSON-APIs-and-AJAX/app.js
@@ -5,8 +5,27 @@ $(document).ready(function() {
       //$(".message").html("Here is the message");
 
       //3.Get JSON with the jQuery getJSON Method
-    $.getJSON("/json/cats.json", function(json) {
-      $(".message").html(JSON.stringify(json));
+        //   $.getJSON("/json/cats.json", function(json) {
+        //       $(".message").html(JSON.stringify(json));
+        //   });
+
+    //4.
+    $("#getMessage").on("click", function() {
+      $.getJSON("/json/cats.json", function(json) {
+
+        var html = "";
+        json.forEach(function(val) {
+          var keys = Object.keys(val);
+          html += "<div class = 'cat'>";
+          keys.forEach(function(key) {
+            html += "<strong>" + key + "</strong>:" + val[key] + "<br>";
+            html += "</div><br>";
+          })
+        })
+
+        $(".message").html(html);
+
+      });
     });
   })
 });
