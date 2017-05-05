f614aae2fa6ce349967a0f1822273cbff94506fc
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-04 21:24:41 -0700

    //14. Seek and Destroy

--- Basic_Algorithm_Scripting/app.js
+++ Basic_Algorithm_Scripting/app.js
@@ -188,3 +188,17 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+
+//14. Seek and Destroy
+
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  args.splice(0,1);
+  return arr.filter(function(element) {
+    return args.indexOf(element) === -1;
+  });
+
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
