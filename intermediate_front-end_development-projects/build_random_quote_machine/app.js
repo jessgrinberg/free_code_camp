4737d846dd5681957949491bb916e36895f454a7
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-08 16:15:25 -0700

    share quote on twitter

--- intermediate_front-end_development-projects/build_random_quote_machine/app.js
+++ intermediate_front-end_development-projects/build_random_quote_machine/app.js
@@ -1,4 +1,5 @@
 var quote = [
+    "If you can dream it, you can do it.",
     "All our dreams can come true, if we have the courage to pursue them.",
     "It's kind of fun to do the impossible.",
     "The way to get started is to quit talking and begin doing.",
@@ -10,11 +11,18 @@ var quote = [
     "Disneyland will never be completed. It will continue to grow as long as there is imagination left in the world."
 ];
 
+tweetUrl = "";
+var forShareQuote = "";
+var randomQuote = quote[0];
+
 function getQuote() {
-    var randomQuote = quote[Math.floor(Math.random() * quote.length)];
+    randomQuote = quote[Math.floor(Math.random() * quote.length)];
     document.getElementById("quoteDisplay").innerHTML = randomQuote;
 }
 
-$("#newQuote").on("click", function() {
-    getQuote();
-});
+function shareQuote() {
+    forShareQuote = randomQuote;
+    console.log(randomQuote);
+    tweetUrl = "https://twitter.com/intent/tweet?text=" + randomQuote;
+    document.getElementById('tweet-share').setAttribute("href", tweetUrl);
+}
