6aec965fbfb3e98421229a68dde5a3a8fb8d62f5
Jessica Grinberg <jessgrinberg@gmail.com>
2017-05-08 21:20:17 -0700

    displayed weather infos

---
+++ intermediate_front-end_development-projects/show_local_weather/app.js
@@ -0,0 +1,25 @@
+  var getIP = 'http://ip-api.com/json/';
+  $.getJSON(getIP).done(function(location) {
+      console.log(location.city)
+      $("#city").html(location.city)
+  })
+
+  var getIP = 'http://ip-api.com/json/';
+  var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
+  $.getJSON(getIP).done(function(location) {
+    $.getJSON(openWeatherMap, {
+        lat: location.lat,
+        lon: location.lon,
+        units: 'metric',
+        APPID: '8ede00cd5fdb826f5e1ee572a29dc74f'
+    }).done(function(weather) {
+        $("#temp").html(Math.round(weather.main.temp));
+        $("#description").html(weather.weather[0].description);
+
+        var iconCode = weather.weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        $("#icon").html("<img src='" + iconUrl  + "'>");
+
+        console.log(weather)
+    })
+})
