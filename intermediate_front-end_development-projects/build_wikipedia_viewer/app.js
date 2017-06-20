d34b0ea2c2cd8be1cef5e28462db4813acc307da
Jessica Grinberg <jessgrinberg@gmail.com>
2017-06-20 16:59:05 -0700

    listed output

--- intermediate_front-end_development-projects/build_wikipedia_viewer/app.js
+++ intermediate_front-end_development-projects/build_wikipedia_viewer/app.js
@@ -12,9 +12,15 @@ $(document).ready(function() {
             async: false,
             dataType: "json",
             success: function(data) {
-                console.log(data[1][0]);
-                console.log(data[2][0]);
-                console.log(data[3][0]);
+                var output = $("#output");
+                for (var i=0; i<data[1].length; i++) {
+                    output.append('<li>' + data[1][0] + '</li>');
+                    output.append('<li>' + data[2][0] + '</li>');
+                    output.append('<li><a href=' +  data[3][0]  + '>'+ data[3][0] +'</li>');
+                }
+                //get heading console.log(data[1][0]);
+                //get description console.log(data[2][0]);
+                //get link console.log(data[3][0]);
             },
             error: function(errorMessage) {
                 alert("Error")
