1ab49196b6d05e0896be311bb3eab93a56170173
Jessica Grinberg <jessgrinberg@gmail.com>
2017-06-20 17:54:41 -0700

    added style

--- intermediate_front-end_development-projects/build_wikipedia_viewer/app.js
+++ intermediate_front-end_development-projects/build_wikipedia_viewer/app.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
                 var output = $("#output");
                 output.html('');
                 for (var i=0; i<data[1].length; i++) {
-                    output.append('<li>' + data[1][0] + '</li>');
-                    output.append('<li>' + data[2][0] + '</li>');
+                    output.append('<li><h4>' + data[1][0] + '</h4></li>');
+                    output.append('<li><h5>' + data[2][0] + '<h5></li>');
                     output.append('<li><a href=' +  data[3][0]  + '>'+ data[3][0] +'</li>');
                 }
                 //get heading console.log(data[1][0]);
