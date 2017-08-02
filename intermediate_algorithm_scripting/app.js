7404aae85cd204c2430226e4bbf928e707a52513
Jessica Grinberg <jessgrinberg@gmail.com>
2017-08-02 08:34:23 -0700

    roman numeral converter

--- intermediate_algorithm_scripting/app.js
+++ intermediate_algorithm_scripting/app.js
@@ -33,3 +33,26 @@ function diffArray(arr1, arr2) {
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+//3.Roman Numeral Converter
+function convertToRoman(num) {
+
+  var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+  var romanized = '';
+
+  for (var index = 0; index < decimalValue.length; index++) {
+    while (decimalValue[index] <= num) {
+      romanized += romanNumeral[index];
+      num -= decimalValue[index];
+    }
+  }
+
+  return romanized;
+}
+
+// test here
+
+convertToRoman(36);
+document.querySelector('#three').innerHTML = convertToRoman(36);
